c1a370de2ad8b3a9c901a4ed0377ae078300694c
wyyalhz <rewardgu011@gmail.com>
2025-04-01 21:02:55 +0800

    Site updated: 2025-04-01 21:02:52

--- js/index.music.js
+++ js/index.music.js
@@ -3,7 +3,7 @@ const core = window._PlayerCore
 core.AppendSongOnTail({
   name: 'Arnheid\'s Theme - やまだ豊', 
   id: 1, 
-  src: '/music/Arnheid\'s Theme - やまだ豊.flac',  // Your audio.
+  src: '/music/Arnheid\'s Theme - やまだ豊.mp3',  // Your audio.
   img: '/images/songs1.png',   // Your thumbnail.
 })
 
@@ -17,20 +17,20 @@ core.AppendSongOnTail({
 core.AppendSongOnTail({
 name: 'attack音D - 澤野弘之', 
   id: 4, 
-  src: '/music/attack音D - 澤野弘之.flac',  // Your audio.
+  src: '/music/attack音D - 澤野弘之.mp3',  // Your audio.
   img: '/images/songs3.png',   // Your thumbnail.
 })
 
 core.AppendSongOnTail({
   name: '夏影 - 麻枝准.flac', 
   id: 3, 
-  src: '/music/夏影 - 麻枝准.flac',  // Your audio.
+  src: '/music/夏影 - 麻枝准.mp3',  // Your audio.
   img: '/images/songs4.png',   // Your thumbnail.
 })
 
 core.AppendSongOnTail({
   name: 'my most precious treasure - 麻枝准', 
   id: 5, 
-  src: '/music/夏影 - 麻枝准.flac',  // Your audio.
+  src: '/music/夏影 - 麻枝准.mp3',  // Your audio.
   img: '/images/songs5.png',   // Your thumbnail.
 })
